83374d3fd44aab56cbb109ba1753c8b8a94372a3
Mihir Mahajan <mihir.mahajan@tum.de>
2024-10-31 19:40:21 +0100

    code cleanup fixes

--- launch/run_bootstrap_distributed.sh
+++ launch/run_bootstrap_distributed.sh
@@ -4,7 +4,5 @@ export REDIS=$(cat redis_hostname_port.txt)
 export DISTRIBUTED=1
 
 # Run the bootstrap script
-# TODO(f.srambical): Make theory a command line argument
 cd learning/
-
-python bootstrap.py theory=nat-add $@
+python bootstrap.py $@
