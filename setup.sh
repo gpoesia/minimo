ae83f4577ff9482481383077c2989a37a3d14bef
Franz Srambical <franz.srambical@gmail.com>
2024-11-03 11:51:56 +0100

    feat: ensure python3.11 being used

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-python3 -m venv .venv
+# FIXME(f.srambical): Ensure that the python version is 3.11
+# python3 -m venv .venv
+
+# Create a virtual environment with python 3.11 and install pip3
+uv sync
 source .venv/bin/activate
+python3 -m ensurepip --upgrade
+
 pip3 install maturin
 cd environment
 maturin dev --release
