952b456d69234db2131815b1fab0d63cf3b6972c
Mihir Mahajan <mihir.mahajan@tum.de>
2024-11-11 18:05:58 +0100

    added light weight model checkpointing

--- learning/bootstrap.py
+++ learning/bootstrap.py
@@ -107,6 +107,7 @@ async def teacher_loop(cfg: DictConfig, mle_log: MLELogger):
 
     with open('log.jsonl', 'w') as log_file:
         for i in range(start_iteration, cfg.agent.policy.total_iterations):
+            torch.save(agent, "model.pt")
             context = Context(d, None, [])
 
             # Dump current agent.
@@ -241,7 +242,6 @@ async def teacher_loop(cfg: DictConfig, mle_log: MLELogger):
             save_json(outcomes, f'outcomes_{i}.json')
 
             save_json(examples, f'examples_{i}.json')
-            # torch.save(student_results, f'results_{i}.json')
 
 
 def prove_conjectures(agent_dump, conjectures, theory, premises):
